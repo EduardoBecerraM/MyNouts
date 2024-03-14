8ba1f101ae412019e056707529b6dd603b5689e8
EduardoBecerraM <beduardo.becerra19@gmail.com>
2024-03-14 13:14:30 -0600

    Clean MainActivity

--- app/src/main/java/com/edebec/mynouts/MainActivity.kt
+++ app/src/main/java/com/edebec/mynouts/MainActivity.kt
@@ -3,13 +3,6 @@ package com.edebec.mynouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.edebec.mynouts.ui.theme.MyNoutsTheme
 
 class MainActivity : ComponentActivity() {
